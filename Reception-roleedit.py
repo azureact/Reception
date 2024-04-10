d4f3afd607ab32fcd0ecf28be09a6e93d9d92c1b
hoah2333 <z13725385251@163.com>
2024-04-11 01:09:10 +0800

    优化排版

--- Reception-roleedit.py
+++ Reception-roleedit.py
@@ -2,23 +2,25 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-allowed_user_ids = [] #允许执行命令的用户列表
+allowed_user_ids = []  # 允许执行命令的用户列表
 
 roles_dict = {}
 
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f"Logged in as {bot.user.name}")
 
-@bot.command(name='roleedit')
+
+@bot.command(name="roleedit")
 async def role_edit(ctx, action, user_id, role_id):
     if ctx.message.author.id not in allowed_user_ids:
         await ctx.send("在权限检查时出现错误：权限不足")
         return
 
-    if action not in ['add', 'del']:
+    if action not in ["add", "del"]:
         await ctx.send("在使用命令时出现错误：无效的参数")
         return
 
@@ -31,11 +33,11 @@ async def role_edit(ctx, action, user_id, role_id):
         await ctx.send("在执行命令时出现错误：无效的用户ID、成员或身份组ID")
         return
 
-    if action == 'add':
+    if action == "add":
         await member.add_roles(role)
         roles_dict.setdefault(str(user.id), []).append(str(role.id))
         await ctx.send(f"已添加*{user.name}*到*{role.name}*身份组")
-    elif action == 'del':
+    elif action == "del":
         if str(role.id) in roles_dict.get(str(user.id), []):
             await member.remove_roles(role)
             roles_dict[str(user.id)].remove(str(role.id))
@@ -43,4 +45,5 @@ async def role_edit(ctx, action, user_id, role_id):
         else:
             await ctx.send(f"错误：*{user.name}*不在*{role.name}*身份组中")
 
-bot.run('')
+
+bot.run("")
